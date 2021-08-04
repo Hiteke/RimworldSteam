e2b1fe251c5199f0dcaa5fd571f46cffa20cff89
Hiteke <38786753+Hiteke@users.noreply.github.com>
2021-08-04 21:06:01 +0900

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using Steamworks;
+using System.Reflection;
+using Verse;
+using Verse.Steam;
+
+namespace Rimworld_Steam
+{
+    [StaticConstructorOnStartup]
+    public class Main
+    {
+        static public bool Initialized;
+        static Main()
+        {
+            var instance = new Harmony("hiteke.steam");
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+            Initialized = SteamAPI.Init();
+        }
+    }
+
+    [HarmonyPatch(typeof(SteamManager))]
+    [HarmonyPatch("Initialized", MethodType.Getter)]
+    class PatchInitialized
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ref bool __result)
+        {
+            __result = Main.Initialized;
+            return false;
+        }
+    }
+}
